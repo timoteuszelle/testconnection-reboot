4e0f13e55e1d3bf20f0d5cc1dd177233455ec834
Tim Zelle <tim.zelle@sublie.me>
2021-01-19 17:23:52 +0100

    needs rework

--- testconnection-reboot/testconnection-reboot.ps1
+++ testconnection-reboot/testconnection-reboot.ps1
@@ -3,18 +3,41 @@ Author: Tim Zelle
 --
 #> 
 Import-Module "C:\Users\Timoteus\Documents\gihub repo\functions\vcenterserver.psm1" -Global
-$file = (get-content "C:\x.txt") #path for your hosts in file
+$file = (get-content "C:\x.txt") 
 
 foreach ($vm in $file) {
 
-$names = Get-VM -name $vm 
-$testrslt =(Test-Connection -Quiet -ComputerName $names -count 2) #test connection true or false
-    if ($testrslt -eq $true){ #check for result true print on screen
-    write-host "Server $names is up."}
-    else {write-host "Server $names is not responding." #if result is not true, will start the reboot sequence.
-    write-host "Server $names will reboot."
-    get-vm -Name $names | Stop-VM -confirm:$false
-    get-vm -Name $names | Start-VM
-    write-host "Server $names booting up."
-    }
-                               }
+    $vmname = Get-VM -name $vm 
+    $testrslt =(Test-Connection -Quiet -ComputerName $vmname -count 2) 
+        if (
+            $testrslt -eq $true)
+            { 
+                write-host "Server $vmname is up."
+            }
+        else    {
+                    write-host "Server $vmname is not responding."
+                
+                    $nicinfo = (get-vm -name $vmname | Get-NetworkAdapter)
+                }
+                    if ($nicinfo.ConnectionState -eq $false) {
+                        get-vm -name $vmname | Get-NetworkAdapter | Set-NetworkAdapter -Connected:$true -StartConnected:$true
+                        Write-Host "NIC is set on disconnected. The NIC will be set to connected and startconnected currently set as $nicinfo.ConnectionState.StartConnected."
+                }
+                    $testrslt =(Test-Connection -Quiet -ComputerName $vmname -count 2) 
+                    if ($testrslt -eq $true){ 
+                    write-host "Server $vmname is up."}
+                    else    {
+                        write-host "Server $vmname is still not responding."
+                        {write-host "Server $vmname will reboot."
+                        {get-vm -Name $vmname | Stop-VM -confirm:$false
+                        get-vm -Name $vmname | Start-VM
+                        write-host "Server $vmname booting up."}
+                    
+                }
+                            elseif (write-host "Server $vmname will reboot.")
+                                {get-vm -Name $vmname | Stop-VM -confirm:$false
+                                get-vm -Name $vmname | Start-VM
+                                write-host "Server $vmname booting up."}
+                        }
+    
+                               
