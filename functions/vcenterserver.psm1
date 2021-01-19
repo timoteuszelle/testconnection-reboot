c391b5b1ec5f54787011306544ff558d1a4f026a
Tim Zelle <tim.zelle@sublie.me>
2021-01-19 15:25:53 +0100

    edits

--- functions/vcenterserver.psm1
+++ functions/vcenterserver.psm1
@@ -3,8 +3,8 @@ Author: Tim Zelle
 --
 #> 
 function vcenterserver {
-Import-Module  "C:\Users\Timoteus\Documents\gihub repo\functions\server-user-vcenter.psm1"  
+Import-Module  "C:\Users\Timoteus\Documents\gihub repo\functions\server-user-vcenter.psm1"  -Global
  Get-Module -ListAvailable *vmware* | Import-Module 
 Add-PSSnapIn vmware*
-Connect-VIServer -Server $vcenterserver -User '$vcenteruser' -pass 'vcenterpassword'
+Connect-VIServer -Server $vcenterserver -User $vcenteruser -pass $vcenterpassword
 }
