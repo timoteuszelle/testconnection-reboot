0ebb56e96a25b814853a7198a303e4b2eafceed3
timoteuszelle <46320568+timoteuszelle@users.noreply.github.com>
2021-03-05 09:06:41 +0100

    fix this
    updated fixes

--- testconnection-reboot/testconnection-reboot.ps1
+++ testconnection-reboot/testconnection-reboot.ps1
@@ -17,37 +17,47 @@ $vcenterserver ="test.domain.ps"
 Get-Module -ListAvailable *vmware* | Import-Module
 Add-PSSnapIn vmware*
 Connect-VIServer -Server $vcenterserver -Credential $credential 
-foreach ($vm in $file) {
-    $vmname = Get-VM -name $vm 
-    $testrslt =(Test-Connection -Quiet -ComputerName $vmname -count 2) 
-        if (
-            $testrslt -eq $true)
-            { 
-                write-host "Server $vmname is up."
-            }
-        else    {
+$log = "c:\temp\connectiontest.log"
+$date = get-date
+foreach ($vm in $file)
+{ 
+        $vmname = Get-VM -name $vm 
+        $testrslt =(Test-Connection -Quiet -ComputerName $vmname -count 2) 
+        if ($testrslt -eq $true)
+        {
+                    write-host "Server $vmname is up."
+                    }
+        else
+        {
                     write-host "Server $vmname is not responding."
-                
                     $nicinfo = (get-vm -name $vmname | Get-NetworkAdapter)
-                }
-                    if ($nicinfo.ConnectionState -eq $false) {
-                        get-vm -name $vmname | Get-NetworkAdapter | Set-NetworkAdapter -Connected:$true -StartConnected:$true
-                        Write-Host "NIC is set on disconnected. The NIC will be set to connected and startconnected currently set as $nicinfo.ConnectionState.StartConnected."
-                }
-                    $testrslt =(Test-Connection -Quiet -ComputerName $vmname -count 2) 
-                    if ($testrslt -eq $true){ 
-                    write-host "Server $vmname is up."}
-                    elseif ($testrslt -eq $false)    {
-                        write-host "Server $vmname is still not responding."
-                        write-host "Server $vmname will reboot."
-                        get-vm -Name $vmname | Stop-VM -confirm:$false
-                        get-vm -Name $vmname | Start-VM
-                        write-host "Server $vmname booting up."}
-                    
-                
-                    else {write-host "Server $vmname will reboot."
-                                get-vm -Name $vmname | Stop-VM -confirm:$false
-                                get-vm -Name $vmname | Start-VM
-                                write-host "Server $vmname booting up."
-                }
-                   }            
+                    if ($nicinfo.ConnectionState.Connected -eq $false) {
+                                                            get-vm -name $vmname | Get-NetworkAdapter | Set-NetworkAdapter -Connected:$true -StartConnected:$true
+                                                            Write-Host "NIC was set on disconnected. The NIC will be set to connected and startconnected on."
+                                                            $testrslt =(Test-Connection -Quiet -ComputerName $vmname -count 2) 
+                                                            if ($testrslt -eq $true) {
+                                                            write-host "Server $vmname is up now."
+                                                            Add-Content $log "$vmname ; $date ; NIC"
+                                                            }
+                                                            
+                                                            
+                                                            else 
+                                                            {
+                                                            write-host "Server $vmname is not responding while NIC is set as connected."
+                                                            write-host "Server $vmname will reboot."
+                                                            get-vm -Name $vmname | Stop-VM -confirm:$false
+                                                            get-vm -Name $vmname | Start-VM
+                                                            write-host "Server $vmname booting up."
+                                                            Add-Content $log "$vmname ; $date"
+                                                            }}
+                                                            
+                    else {
+                    write-host "Server $vmname is not responding while NIC is set as connected."
+                    write-host "Server $vmname will reboot."
+                    get-vm -Name $vmname | Stop-VM -confirm:$false
+                    get-vm -Name $vmname | Start-VM
+                    write-host "Server $vmname booting up."
+                    Add-Content $log "$vmname ; $date"
+                    }
+                    }
+                    }         
